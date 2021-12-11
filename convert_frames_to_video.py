5ec20f1683dad37c7a77a296cbfc9913424a3ad2
22969541+Aurelius333@users.noreply.github.com <22969541+Aurelius333@users.noreply.github.com>
2021-12-11 18:22:00 -0500

    Don't allow FPS not to be specified in convert_frames_to_video

--- convert_frames_to_video.py
+++ convert_frames_to_video.py
@@ -1,8 +1,6 @@
 import os
 
-def convert_frames_to_video(input_path, output_path, fps=None):
-    fps_option = "" if fps is None else "-r {fps}"
-
+def convert_frames_to_video(input_path, output_path, fps):
     # -pix_fmt yuv420p: https://superuser.com/questions/820134/why-cant-quicktime-play-a-movie-file-encoded-by-ffmpeg
     # -vf: https://stackoverflow.com/a/29582287
-    os.system(f'ffmpeg {fps_option} -i "{input_path}/%09d.png" -vf "crop=trunc(iw/2)*2:trunc(ih/2)*2" -y -pix_fmt yuv420p "{output_path}"')
+    os.system(f'ffmpeg -r {fps} -i "{input_path}/%09d.png" -vf "crop=trunc(iw/2)*2:trunc(ih/2)*2" -y -pix_fmt yuv420p "{output_path}"')
