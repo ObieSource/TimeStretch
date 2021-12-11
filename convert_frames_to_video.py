d61cf59c82f868ffbfea21f4f406d0e956d93be1
22969541+Aurelius333@users.noreply.github.com <22969541+Aurelius333@users.noreply.github.com>
2021-12-11 18:09:45 -0500

    Don't specify fps option if not specified to functions

--- convert_frames_to_video.py
+++ convert_frames_to_video.py
@@ -1,6 +1,8 @@
 import os
 
-def convert_frames_to_video(input_path, output_path, fps=30):
-	# -pix_fmt yuv420p: https://superuser.com/questions/820134/why-cant-quicktime-play-a-movie-file-encoded-by-ffmpeg
-	# -vf: https://stackoverflow.com/a/29582287
-	os.system(f'ffmpeg -r {fps} -i "{input_path}/%09d.png" -vf "crop=trunc(iw/2)*2:trunc(ih/2)*2" -y -pix_fmt yuv420p "{output_path}"')
+def convert_frames_to_video(input_path, output_path, fps=None):
+    fps_option = "" if fps is None else "-r {fps}"
+
+    # -pix_fmt yuv420p: https://superuser.com/questions/820134/why-cant-quicktime-play-a-movie-file-encoded-by-ffmpeg
+    # -vf: https://stackoverflow.com/a/29582287
+    os.system(f'ffmpeg {fps_option} -i "{input_path}/%09d.png" -vf "crop=trunc(iw/2)*2:trunc(ih/2)*2" -y -pix_fmt yuv420p "{output_path}"')
--- convert_video_to_frames.py
+++ convert_video_to_frames.py
@@ -1,11 +1,12 @@
 import os
 import shutil
 
-def convert_video_to_frames(input_path, output_path, fps=30):
+def convert_video_to_frames(input_path, output_path, fps=None):
     if os.path.exists(output_path):
         print(f'Deleting directory {output_path}')
         shutil.rmtree(output_path)
     print(f'Creating directory {output_path}')
     os.makedirs(output_path)
 
-    os.system(f'ffmpeg -i "{input_path}" -vf fps={fps} "{output_path}/%09d.png"')
+    fps_option = "" if fps is None else "-vf fps={fps}"
+    os.system(f'ffmpeg -i "{input_path}" {fps_option} "{output_path}/%09d.png"')
